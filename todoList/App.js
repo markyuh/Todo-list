84983c76fe9d49bef36c3d090fdfef923226ec5c
markyuh <markbay@colostate.edu>
2024-07-31 14:36:05 -0600

    Created title and Task component

--- todoList/App.js
+++ todoList/App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}> 
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style = {styles.items}>
+          {/* this is where the tasks will go */}
+          <Task/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
+  tasksWrapper : {},
+  sectionTitle : {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items : {},
 });
